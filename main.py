38ac9c47ba7c11aab11d26dd42371d9ba3af07fb
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2022-07-13 14:03:00 +0200

    Modification finale

---
+++ main.py
@@ -0,0 +1,14 @@
+from scrape_images import scrape_books_images_from
+from scrape_informations import scrape_books_informations_from
+
+web_site_url = "http://books.toscrape.com/"
+
+menu = f"""------------------------------------------------------------------------------------------------------------
+Bienvenue dans notre application qui nous permet via le lien {web_site_url} de :
+1. Extraire les informations de tous les livres pour chaque catégorie et les transformer en un fichier CSV
+2. Télécharger et enregistrer toutes les images de livres pour chaque catégorie sous forme des fichiers JPG
+------------------------------------------------------------------------------------------------------------"""
+
+print(menu)
+scrape_books_informations_from(web_site_url)
+scrape_books_images_from(web_site_url)
